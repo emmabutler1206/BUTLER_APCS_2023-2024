26aec939f9de378804e7ab4a6bf5f0f385605cd2
emmabutler1206 <butler.emma.24@c-dh.org>
2023-09-12 14:38:01 -0500

    These files are for homework assigned for 9/15

---
+++ 002_FamousQuote/FamousQuote.java
@@ -0,0 +1,11 @@
+public class FamousQuote
+{ 
+    public static void main(String[] args)
+    {
+System.out.println("\"You're a wizard, Harry\"");
+System.out.println("-Hagrid");
+    }
+}
+    
+    
+     
