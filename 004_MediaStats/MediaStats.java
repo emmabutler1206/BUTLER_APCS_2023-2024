90d04703cd6d156f508f3ad909b21d3daab5d5ea
emmabutler1206 <butler.emma.24@c-dh.org>
2023-09-15 13:03:29 -0500

    For Future Assignment

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,17 @@
+public class MediaStats
+{ 
+    public static void main(String[] args)
+    { 
+        int x= 152;
+        int a= 2001;
+        int y= 161;
+        int b= 2004;
+        int z= 142;
+        int c= 2005;
+        System.out.println("Harry Potter and the Sorcerer's Stone was released on " + a + " and is " + x + " minutes long." );
+        System.out.println( "Harry Potter and the Chamber of Secrets was released on " + b + " and is " + y + " minutes long.");
+        System.out.println("Harry Potter and the Prizoner of Azkaban was released on  ")
+}
+
+
+}
