ad6e4715589d651c336b50de0da6324b7679b7d9
emmabutler1206 <143751125+emmabutler1206@users.noreply.github.com>
2023-09-27 00:53:32 +0000

    Used on assignment 10

---
+++ 010_ComputeAnExpression/ComputeAnExpression.java
@@ -0,0 +1,17 @@
+public class ComputeAnExpression
+  {
+    public static void main(String[] args)
+    {
+      double x= 9.5;
+      double y= 4.5;
+      double z= 2.5;
+      double a= 45.5;
+      double b= 3.5;
+      int c= 3;
+        
+      System.out.println((double)(x*y-          z*c)/(a-b));
+      //Practiced using doubles
+    }
+  
+    
+  }
