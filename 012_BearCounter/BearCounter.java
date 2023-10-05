22701fb0743afc3cfa350ab71a69dc7c7636d9d1
emmabutler1206 <butler.emma.24@c-dh.org>
2023-10-05 13:51:56 -0500

    completed assignments

--- 012_BearCounter/BearCounter.java
+++ 012_BearCounter/BearCounter.java
@@ -17,7 +17,9 @@ public class BearCounter
         System.out.println("Oh my, there is " + numOfBears + " bear in the clearing!" );
         numOfBears-=1;
         System.out.println("Oh my, there are " + numOfBears + " bears in the clearing!" );
+  //Created Variables with assignment operators.
     }
+  
   }
    
   
---
+++ 013_NameData/NameData.java
@@ -0,0 +1,17 @@
+public class NameData
+{
+    public static void main(String[] args)
+    {
+       NameTransmogrifier me = new NameTransmogrifier("me");
+        System.out.println(me.name);
+        System.out.println(me.nameUpperCase);
+        System.out.println(me.nameLowerCase);
+        System.out.println(me.nameReversed);
+        System.out.println(me.nameLength);
+        System.out.println(me.nameFirstLetter);
+        System.out.println(me.nameLastLetter);
+//Used an object
+    }
+        
+    
+}
