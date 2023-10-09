d5321573f2f0bc8f88357d8a62ce3cb5aa73d742
emmabutler1206 <butler.emma.24@c-dh.org>
2023-10-09 11:45:47 -0500

    All current assignments

---
+++ 014_SweetGarage/SweetGarage.java
@@ -0,0 +1,24 @@
+public class SweetGarage
+{
+    public static void main(String[] args)
+    {
+       Car firstCar= new Car( 2023, 1_000, "Mercedes", "Benz", true);
+       Car secondCar= new Car(2021, 500, "Ferrari", "F8 Tributo", true);
+       Car thirdCar= new Car (2020, 400, "Range Rover", "Land Rover");
+       Car fourthCar=new Car (2019, 300, "Corvette", "Stingray");
+       Car fifthCar= new Car (2018, 5, "Honda", "Accord");
+       System.out.println(firstCar.manufacturerName + " " + firstCar.milesDriven + " " + firstCar.year +" " + firstCar.modelName);
+        System.out.println(secondCar.manufacturerName + " " + secondCar.milesDriven + " " + secondCar.year + " " + secondCar.modelName);
+        System.out.println(thirdCar.manufacturerName + " " + thirdCar.milesDriven + " " + thirdCar.year + " " + thirdCar.modelName);
+        System.out.println(fourthCar.manufacturerName + " " + fourthCar.milesDriven + " " + fourthCar.year + " " + fourthCar.modelName);
+        System.out.println(fifthCar.manufacturerName + " " + fifthCar.milesDriven + " " + fifthCar.year + " " + fifthCar.modelName);
+        firstCar.revEngine();
+         firstCar.engageTurbo();
+        secondCar.revEngine();
+        secondCar.engageTurbo();
+        //Created a garage of cars using an already existing object.
+    }
+
+    
+    
+}
--- 015_TurtleBoxer/Java-Swing-Turtle/Main.java
+++ 015_TurtleBoxer/Java-Swing-Turtle/Main.java
@@ -3,8 +3,41 @@ import java.awt.event.*;
 import java.util.*;
 import java.awt.*;
 
-public class Main {
-  public static void main(String[] args) {
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    World habitat= new World(220, 220);
+    Turtle Bob = new Turtle(habitat);
+    Bob.forward();
+    Bob.turnLeft();
+    Bob.forward();
+    Bob.turnLeft();
+    Bob.forward();
+    Bob.turnLeft();
+    Bob.forward();
+    Bob.forward();
+    Bob.turnLeft();
+    Bob.forward();
+    Bob.turnLeft();
+    Bob.forward();
+    Bob.turnLeft();
+    Bob.forward();
+    Bob.forward();
+    Bob.turnRight();
+    Bob.forward();
+    Bob.turnRight();
+    Bob.forward();
+    Bob.turnRight();
+    Bob.forward();
+    Bob.forward();
+    Bob.turnRight();
+    Bob.forward();
+    Bob.turnRight();
+    Bob.forward();
+    habitat.setVisible(true);
+
+// followed all the instructions using different classes.
     //Refer back to your Runestone academy book as needed.
     //Instantiate the world. The world should be 220x220.
     
