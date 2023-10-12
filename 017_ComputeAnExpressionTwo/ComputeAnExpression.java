74d80ae5b58c25c5eef6f3b2442f5803b654422a
emmabutler1206 <butler.emma.24@c-dh.org>
2023-10-12 13:49:09 -0500

    Assignments on 10/12

---
+++ 017_ComputeAnExpressionTwo/ComputeAnExpression.java
@@ -0,0 +1,25 @@
+public class ComputeAnExpression
+  {
+    public static void main(String[] args)
+    {
+      ReturningCalculator calc = new ReturningCalculator();
+      double variableOne= 9.5;
+      double variableTwo= 4.5;
+      double variableThree= 2.5;
+      double variableFour= 45.5;
+      double variableFive= 3.5;
+      double variableSix= 3.0;
+      double variableSeven= calc.doubleMultiplier(variableOne, variableTwo);
+      double variableEight= calc.doubleMultiplier(variableThree, variableSix);
+      double variableNine= calc.doubleSubtractor(variableSeven, variableEight);
+      double variableTen= calc.doubleSubtractor(variableFour, variableFive);
+      double variableEleven= calc.divider(variableNine, variableTen);
+      System.out.println(variableEleven);
+        
+      
+      //Used a pre-existing project and integrated methods from a class
+
+    }
+  
+    
+  }
