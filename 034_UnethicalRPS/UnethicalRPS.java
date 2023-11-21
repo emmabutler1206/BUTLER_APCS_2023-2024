85e6b979a09f29c0e0f08ebaaa02ccb27acf509c
emmabutler1206 <butler.emma.24@c-dh.org>
2023-11-21 10:26:44 -0600

    Homework

---
+++ 034_UnethicalRPS/UnethicalRPS.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class UnethicalRPS
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String userInput = "";
+        String first = "rock";
+        String second = "paper";
+        String third = "scissors";
+        System.out.println("Please eneter your move in lowercase letters");
+        scan.nextLine();
+        scan.close();
+        
+        if(userInput.equals(first))
+        {
+            System.out.println("The computers choice is paper");
+           System.out.println("you loose");
+            
+        }
+        else if(userInput.equals(second))
+        {
+            System.out.println("The computers choice is scissors"); 
+            System.out.println("You loose");
+        }
+         else
+        {
+            System.out.println("The computers choice is rock");
+            System.out.println("you loose");
+        }
+            
+            
+           
+
+    }
+}
