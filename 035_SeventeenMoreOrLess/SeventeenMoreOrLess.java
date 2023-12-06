a4235386bae1366a4a58db8786aeef64c63a4788
emmabutler1206 <butler.emma.24@c-dh.org>
2023-12-06 14:47:45 -0600

    Missing Homework

---
+++ 035_SeventeenMoreOrLess/SeventeenMoreOrLess.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+public class SeventeenMoreOrLess
+{
+    public static void main(String[] args)
+    {
+        int one = 0;
+        int two = 0;
+        int three = 0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter in your first integer");
+        one = scan.nextInt();
+        System.out.println("Enter in your second integer");
+        two = scan.nextInt();
+        System.out.println("Enter in your third integer");
+        three = scan.nextInt();
+        scan.close();
+        // used for first integer
+        if(one >= two+17 || one <= two-17)
+        {
+            System.out.println("The first integer is 17 more or less than the second integer");
+        }
+        else
+        {
+            System.out.println("The first integer is not 17 more or less than the second integer ");
+        }
+
+        if(one >= three+ 17 || one<=three-17) 
+        {
+            System.out.println("The first integer is 17 more or less than the third integer");
+        }
+        else
+        {
+            System.out.println("The first integer is not 17 more or less than the third integer");
+        }
+
+        if (two>=one+17 || two<=one-17)
+        {
+            System.out.println("The second integer is 17 more or less than the first integer");
+        }
+        else 
+        {
+            System.out.println("The second integer is not 17 more or less than the first integer");
+        }
+    
+        if(two>=three+17 || two<=three-17)
+        {
+            System.out.println("The second integer is 17 more or less than the third integer");
+        }
+        else 
+        {
+            System.out.println("The second integer is not 17 more or less than the third integer");
+        }
+        
+        if (three>=one+17 || three<=one-17)
+        {
+            System.out.println("The third integer is 17 more or less than the first integer");
+        }
+        else 
+        {
+            System.out.println("The third integer is not 17 more or less than the first integer");
+        }
+        
+        
+        if(three>=two+17 || three<=two-17)
+        {
+            System.out.println("The third integer is 17 more or less than the second integer");
+        }
+        else
+        {
+            System.out.println("The third integer is not 17 more or less than the second integer");
+        }
+    }
+}
+// Used Or statements to take imputs from user, and generate the correct repsonse back
---
+++ 036_CommonDidgit/CommonDidgit.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class CommonDidgit
+{
+    public static void main(String []args)
+    {
+       Scanner scan = new Scanner(System.in);
+        int userIntegerOne = 0;
+        int userIntegerTwo= 0;
+
+        
+        System.out.println("Enter your first integer betwen 25 and 75");
+        userIntegerOne =  scan.nextInt();
+        System.out.println("Enter your second integer betwen 25 and 75");
+        userIntegerTwo = scan.nextInt();
+        
+       
+       Integer numberOneObject = Integer.valueOf(userIntegerOne);
+       Integer numberTwoObject = Integer.valueOf(userIntegerTwo);
+       String numberOneString= numberOneObject.toString();
+        String numberTwoString = numberTwoObject.toString();
+
+
+        if (userIntegerOne <= 25 || userIntegerTwo <= 25)
+        {
+            System.out.println("Error");
+        }
+        else if (userIntegerOne >=75 || userIntegerTwo >=75)
+        {
+            System.out.println("Error");
+        }
+        else
+        {
+         
+            if (numberOneString.substring(1).equals(numberTwoString.substring(1)))
+           {
+            System.out.println("The shared integer is " + numberTwoString.substring(1));
+           }
+           if (numberOneString.substring(0).equals(numberTwoString.substring(0)))
+           {
+            System.out.println("The shared integer is " + numberTwoString.substring(0));
+           }
+        }
+
+    }
+}
+//USED IF STATEMENTS AND MATH OPERATORS TO TAKE A USERS INPUT AND SHOVE OUT THE CORRECT OUTPUT
