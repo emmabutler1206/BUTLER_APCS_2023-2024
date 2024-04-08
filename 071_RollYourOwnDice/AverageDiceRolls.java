44215952f5cedb27776593b072b4686bec6e995a
emmabutler1206 <143751125+emmabutler1206@users.noreply.github.com>
2024-04-08 14:53:51 -0500

    hw

---
+++ 071_RollYourOwnDice/AverageDiceRolls.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class AverageDiceRolls {
+    
+   
+       
+        ArrayList<Integer> diceRoll= new ArrayList<Integer>();
+        diceRoll.add(Dice.getValue());
+        
+        public void DiceArrayList()
+        {
+            for(int i=0; i<diceRoll.size(); i++)
+            {
+               sum = sum + diceRoll.add[i];
+               
+               
+            }
+            sum/diceRoll.size()= average;
+
+            System.out.println("Would you like to roll the dice again?");
+            if(userImput == "yes")
+            {
+
+            }
+            else if(userImput= "no")
+            {
+                System.out.println("Here is your dice average:" + average);
+            }
+    }
+}
+
+  
+      
+
+
+
+
+    
+
---
+++ 071_RollYourOwnDice/Dice.java
@@ -0,0 +1,21 @@
+public class Dice {
+    public int value; 
+    public int numOfSides;
+
+
+    public Dice(int numOfSides)
+    {
+        this.value= value;
+        this.numOfSides=numOfSides;
+    }
+    
+    public int getValue()
+    {
+        return value;
+    }
+    public int getNumOfSides()
+    {
+        return numOfSides;
+    }
+    public static void main(String[] args){}
+}
