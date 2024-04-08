8ab6521594331e3769f94c06b8c7504a3269c75f
dgsobolewski <dgsobolewski@gmail.com>
2024-04-08 09:33:30 -0500

    Comments on Power Attack

--- 065_PowerAttackTwo/Hero.java
+++ 065_PowerAttackTwo/Hero.java
@@ -11,13 +11,13 @@ public class Hero
 // Hero Class has methods and variables that describe the attributes of a hero.
     public Hero( String name, double health, double armourHealth, int strength, String magicType)
     {
-    this.name = name;
-    this.health= health;
-    this.armourHealth = armourHealth;
-    this.strength= strength;
-    this.magicType=magicType;
-    this.randomAttack= randomAttack;
-    this.randomAttack= randomAttack;
+        this.name = name;
+        this.health= health;
+        this.armourHealth = armourHealth;
+        this.strength= strength;
+        this.magicType=magicType;
+        this.randomAttack= randomAttack;
+        this.randomAttack= randomAttack;
     }
     
     public static void main(String[] args){}
@@ -32,15 +32,12 @@ public class Hero
            int randomAttack = randomRoll + strength;
            System.out.println("You now have " + randomAttack + " points");
 
-
            if(randomAttack > armourHealth) {
                int randomDamage =(int)(Math.random()*2)+4;
                hit -= randomDamage;
                System.out.println("The monster has " + hit + " points left");
            }
-          
-
-    }
+        }
     }
 // Tells us if the hero has won or lost based upon the cheers
     public void Cheer(int monsterHealth)
@@ -55,5 +52,3 @@ public class Hero
         }
     }
 }
-
-    
