2ad1a3085c0f14feb2e1ed3e1e541c066a33811e
emmabutler1206 <143751125+emmabutler1206@users.noreply.github.com>
2024-05-23 12:04:11 -0500

    table

--- 079_Foods/Table.java
+++ 079_Foods/Table.java
@@ -1,5 +1,14 @@
 public class Table {
-    
-}
-
+    // creating in main
+    public static void main(String[] args) {
+      // initializing main class
+      Food newFood = new Food();
+  
+      // creating new cases of both pen and pencil to enable them to print in thr main
+      // method
+      Grapes foodOne = new Grapes();
+      System.out.println(foodOne);
+  
+    }
+  }
 /* 0/5 incomplete */
